9d3642ad40e5a86aaa4306e9270b980970118c55
zirconiium <63737942+zirconiium@users.noreply.github.com>
2021-03-04 17:26:03 +0100

    [discord] cleaned up module

--- client/modules/discord/discord.js
+++ client/modules/discord/discord.js
@@ -4,8 +4,5 @@ zFramework.Modules.Discord.Initialize = function() {
 	SetDiscordRichPresenceAssetText("SantosRP");
 	SetDiscordRichPresenceAssetSmall("discord_512");
 	SetDiscordRichPresenceAssetSmallText("discord.santosrp.fr");
-	this.SetRichPresence(`[${GetPlayerServerId(zFramework.LocalPlayer.playerId)}] ${`${zFramework.LocalPlayer.identity.firstname} ${zFramework.LocalPlayer.identity.lastname}` || zFramework.LocalPlayer.name}`);
-    this.Initialized = true;
+	SetRichPresence(`[${GetPlayerServerId(zFramework.LocalPlayer.playerId)}] ${`${zFramework.LocalPlayer.identity.firstname} ${zFramework.LocalPlayer.identity.lastname}` || zFramework.LocalPlayer.name}`);
 };
-
-zFramework.Modules.Discord.SetRichPresence = status => SetRichPresence(status);
