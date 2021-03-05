6376458bb07318324f28bcaa819306cf401439bb
zirconiium <63737942+zirconiium@users.noreply.github.com>
2021-03-05 14:30:10 +0100

    [needs] added new var condition

--- client/core/needs/needs.js
+++ client/core/needs/needs.js
@@ -16,7 +16,6 @@ DrawNeedsBar = (x, y, width, height, color) => DrawRect(x + width / 2, y + heigh
 zFramework.Core.Needs.Think = function() {
     if (!zFramework.LocalPlayer.cinemaMode) {
         const { hunger, thirst } = zFramework.LocalPlayer.needs;
-        
         DrawNeedsBar(0.016 - .001, this.Y - 0.015, 0.14, .014, [247, 197, 59, 75]);
         DrawNeedsBar(0.016 - .001, this.Y - 0.015, (hunger / 100) * .14, .014, [247, 197, 59, 155]);
         DrawNeedsBar(0.016 - .001, this.Y, .14, .014, [31, 162, 255, 75]);
@@ -25,13 +24,14 @@ zFramework.Core.Needs.Think = function() {
 }
 
 zFramework.Core.Needs.Thread = function(localPlayer) {
-    if (!localPlayer.dead && !localPlayer.spectateMode && IsPedHuman(localPlayer.pedId)) serverEvent("Server.Needs.Thread");
+    if (!localPlayer.dead && localPlayer.busy <= 1 && !localPlayer.spectateMode && IsPedHuman(localPlayer.pedId)) serverEvent("Server.Needs.Thread");
 }
 
 zFramework.Core.Needs.OnUpdated = function() {
-    if (zFramework.LocalPlayer.needs.thirst <= 0 || zFramework.LocalPlayer.needs.hunger <= 0) {
+    const { hunger, thirst } = zFramework.LocalPlayer.needs;
+    if (thirst <= 0 || hunger <= 0) {
         this.Reset(true);
-        zFramework.Functions.Notify(`~r~Vous êtes mort de ~b~${zFramework.LocalPlayer.needs.hunger <= 0 ? "faim" : "soif"}~w~.`);
-    } else if (zFramework.LocalPlayer.needs.thirst < 10 || zFramework.LocalPlayer.needs.hunger < 10)
+        zFramework.Functions.Notify(`~r~Vous êtes mort de ~y~${hunger <= 0 ? "faim" : "soif"}~w~.`);
+    } else if (thirst < 10 || hunger < 10)
         zFramework.Functions.Notify("~y~Vous avez mal au ventre.");
 }
