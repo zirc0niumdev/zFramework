061817db415d0f7f79f8c4740900d8295e9636fb
zirconiium <63737942+zirconiium@users.noreply.github.com>
2021-03-11 08:23:59 +0100

    [client] formatted gameEventTriggered

--- client/init.js
+++ client/init.js
@@ -3,26 +3,24 @@ import CLocalPlayer from './class/CLocalPlayer.js';
 RegisterCommand("respawn", () => serverEvent("Server.GeneratePlayer"));
 
 onNet('Client.CreatePlayer', tempPlayerData => {
-	if (zFramework.LocalPlayer) return;
+	if (zFramework.LocalPlayer); //return serverEvent("Server.Kick", "Une erreur à été rencontrée lors de votre connexion. Code Erreur: error-player-already-exists");
 	zFramework.LocalPlayer = new CLocalPlayer(tempPlayerData);
 });
 
 on("gameEventTriggered", (name, args) => {
-    switch (name) {
-        case "CEventNetworkEntityDamage":
-            let i = 0;
-            const victim = args[i++];
-            const instigator = args[i++];
-      
-            i++; // skip unknown value
-            i++; // skip unknown 2060 new boolean value
-            i++; // skip unknown 2189 new boolean value
-      
-            const isFatal = !!args[i++];
-            const weaponHash = args[i++];
+    if (name == "CEventNetworkEntityDamage") {
+        let i = 0;
+        const victim = args[i++];
+        const instigator = args[i++];
+  
+        i++; // skip unknown value
+        i++; // skip unknown 2060 new boolean value
+        i++; // skip unknown 2189 new boolean value
+  
+        const isFatal = !!args[i++];
+        const weaponHash = args[i++];
 
-            // consume data
-            zFramework.Core.LSMS.OnEntityDamage(victim, instigator, isFatal, weaponHash);
-            break;
+        // consume data
+        zFramework.Core.LSMS.OnEntityDamage(victim, instigator, isFatal, weaponHash);
     }
 });
